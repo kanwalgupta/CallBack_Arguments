41ed80257fdb90f72b8324bae41b3e124a572289
kanwalgupta <kanwal.gupta1@gmail.com>
2018-08-02 18:27:28 +0000

    Restructured function callbacks

--- function-callbacks.js
+++ function-callbacks.js
@@ -1,10 +1,13 @@
 // The second argument/parameter is expected to be a function
 
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
+  function findWaldo(arr, found) {
+    arr.forEach(function(item) {
+      if (item === "Waldo") {
+        found(arr.indexOf(item));
+      }
+    });
   }
+
 
 
 function actionWhenFound(index) {
